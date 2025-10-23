8eb57737a12370033e7b4ff83e2d7a8922fd7099
Tomás Benjamín Sepúlveda Oñate <tomasbso.cb@gmail.com>
2025-10-23 13:28:49 -0300

    Dialogic Placeholder

---
+++ scripts/scenarios/library_1.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _ready() -> void:
+	Dialogic.start("library1")
--- scripts/ui/game_ui.gd
+++ scripts/ui/game_ui.gd
@@ -1,4 +1,15 @@
 extends Control
 
+@onready var animations: AnimationPlayer = $AnimationPlayer
+
 func _on_exit_pressed() -> void:
 	Utils.game_controller.return_to_main_menu()
+
+func _on_menu_button_pressed() -> void:
+	animations.play("open_game_menu")
+
+func _on_close_menu_pressed() -> void:
+	animations.play("close_game_menu")
+
+func _on_dialogue_history_pressed() -> void:
+	pass # Replace with function body.
